4e77b2c76cf56befc954a42bb6e3ca88f6a63fbb
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-09-08 14:11:35 -0300

    Input a não funfar

--- Assets/Scripts/MeleeHandler.cs
+++ Assets/Scripts/MeleeHandler.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MeleeHandler : MonoBehaviour
 {
     public Collider2D[] inRange;
-    private float rangeRadius = 4f;
+    [SerializeField] private float rangeRadius = 4f;
     [SerializeField] private LayerMask layerMask;
+    private PlayerControls input;
     
-    private void Update() 
-    { 
+    private void Awake() 
+    {
+        input = new PlayerControls();
+    }
+
+    private void OnEnable() 
+    {
+        input.Enable();
+        input.Player.Melee.started += OnMelee(inRange);  
+    }
+
+    private void OnDisable() 
+    {
+        input.Disable();
+        input.Player.Melee.started -= OnMelee(inRange);
+    }
+
+    public void OnMelee(Collider2D[] inRange, InputAction.CallbackContext ctx)
+    {
         inRange = Physics2D.OverlapCircleAll(gameObject.transform.position, rangeRadius, layerMask);
+        if (inRange.Length >=1 )
+        {
+            Debug.Log("Inimigo detectado.");
+        }
     }
 
     
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,34 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
     private Vector2 _smoothenedMovimentInput, _smootheningVelocity;
+    private PlayerControls input;
 
     public float speed = 4.5f;
 
     private void Awake()
     {
         _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        input = new PlayerControls();
     }
 
-    private void FixedUpdate()
-    {
+    private void Anda(InputAction.CallbackContext ctx)
+    {   
+        _movementInput = ctx.ReadValue<Vector2>();
         _smoothenedMovimentInput = Vector2.SmoothDamp(_smoothenedMovimentInput, _movementInput, ref _smootheningVelocity, 0.1f);
         _rigidbody.velocity = _smoothenedMovimentInput * speed;
     }
 
-    private void OnMove(InputValue inputvalue)
+    private void OnEnable() 
     {
-        _movementInput = inputvalue.Get<Vector2>();
+        input.Enable();
+        input.Player.Move.performed += Anda;
+        input.Player.Move.canceled += Anda;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Move.canceled -= Anda;
+        input.Player.Move.performed -= Anda;
     }
 }
